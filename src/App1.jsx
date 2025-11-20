0e3581aad390116869b109735d1744a03baa864a
Narenthiran26 <narenthiranarul@gmail.com>
2025-11-20 20:48:25 +0530

    third commit

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,24 +1,25 @@
 import './App.css';
 import { useState } from 'react';
 
-function ClickCount(){
-  let [count,setCount]=state(0);
-  return count;
-}
+function MyButton(){
+  let [count,setCount]=useState(0);
 
-function ClickHandle(count){
-  return(
-    ClickCount(count+1)
-  );
+function handleClick(){
+    setCount(count+1);
+ }
+ return (
+  <button onClick={handleClick}>
+    Clicked {count} times
+  </button>
+ );
 }
-
 export default function App() {
   return (
     <div>
-      <h2>Press</h2>
-      <ClickHandle />
+      <h1>Click me</h1>
+      <MyButton />
     </div>
-  )
+  );
 }
 
 
